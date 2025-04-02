f50467b5d501f57d71b0e73682506e8e951aaf6d
kamonoritoshi <huybttps40336@gmail.com>
2025-04-02 18:59:59 +0700

    Commit project SOF3061_ASM to GitHub

---
+++ src/main/java/com/poly/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.poly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/index")
+	public String index(Model model) {
+		return "index";
+	}
+}
